4f08cafb47937d2176a353a6b899137e11bef795
alex-anie <ocxigin@gmail.com>
2025-08-09 21:23:52 +0100

    The Everything Episode

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Employer;
+use App\Models\Job;
+use PHPUnit\Framework\TestCase;
+
+class JobTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        // Arrange
+       $employer = Employer::factory()->create();
+       $job = Job::factory()->create([
+        'employer_id' => $employer-> id
+       ]);
+
+       // Act and Assert
+       expect($job->employer->is($employer))->toBeTrue();
+    }
+}
